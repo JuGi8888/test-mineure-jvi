dc9a99c868df96ad38538b9d495c247108d1bef0
JuGi8888 <julienoty@gmail.com>
2025-10-21 10:16:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ data = pd.DataFrame(d)
 
 st.dataframe(data)
 
-st.header("Widgets Interactifs (partie du cours")
+st.header("Widgets Interactifs (partie du cours)")
 
 # Un slider
-age = st.slider("Quel est votre âge ?", 0, 100, 25)
-st.write("Votre âge est :", age)
+love = st.slider("A quel point vous aimez Julie ?", 0, 100, 25)
+st.write("Vous aimez Julie à :", love)
 
 # Une liste de sélection
 option = st.selectbox(
-    'Quelle est votre couleur préférée ?',
-    ('Bleu', 'Rouge', 'Vert'))
-st.write('Votre couleur préférée est :', option)
+    'Quand est-ce que vous aimeriez voir Julie?',
+    ('Dès que possible <3', 'Quand on pourra', 'Jamais elle gaze'))
+st.write('Vous aimeriez voir Julie.... :', option)
 
 # Un bouton
 if st.button('Cliquez ici !'):
-    st.write('Vous avez cliqué ! Bravo !')
+    st.write('Vous avez cliqué ! Bravo vive JULIE !!')
