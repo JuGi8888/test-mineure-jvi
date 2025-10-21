5984c7830cc4cafd9d9ff17994661406b5ba73a6
JuGi8888 <julienoty@gmail.com>
2025-10-21 09:39:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 import streamlit as st
 
+import pandas as pd
 st.header ("Mon application jvi")
 
-st.write("VIEIRA")
+nombres = [1, 2, 4, 7]
+carre = [1**2, 2**2, 4**2, 7**2]
+
+d = {"nombres": nombres, "carre": carre}
+data = pd.DataFrame(d)
+
+st.dataframe(data)
+
+st.header("Widgets Interactifs (partie du cours")
+
+# Un slider
+age = st.slider("Quel est votre âge ?", 0, 100, 25)
+st.write("Votre âge est :", age)
+
+# Une liste de sélection
+option = st.selectbox(
+    'Quelle est votre couleur préférée ?',
+    ('Bleu', 'Rouge', 'Vert'))
+st.write('Votre couleur préférée est :', option)
+
+# Un bouton
+if st.button('Cliquez ici !'):
+    st.write('Vous avez cliqué ! Bravo !')
